5d2dc83c75fd195a2977ee7742f1954c1aaf3f8d
JoseFrancof3 <is693306@iteso.mx>
2015-10-13 08:10:53 -0500

    lol

--- src/main/java/com/iteso/factory/stores/AllPozolesStore.java
+++ src/main/java/com/iteso/factory/stores/AllPozolesStore.java
@@ -10,27 +10,25 @@ import java.net.InetSocketAddress;
  * Created by JoseFranco on 13/10/2015.
  */
 public class AllPozolesStore extends PozoleStore {
-
-    PozoleStore pozole;
     Pozole pozolestore;
 
     @Override
     protected Pozole createPozole(String meat) {
         if (getType().equals("Pozole Blanco")) {
-            pozole = new PozoleBlancoStore();
-            pozolestore = pozole.orderPozole(meat);
+            PozoleBlancoStore pozoleStore = new PozoleBlancoStore();
+            pozolestore = pozoleStore.createPozole(meat);
         }
         if (getType().equals("Pozole Rojo")) {
-            pozole = new PozoleRojoStore();
-            pozolestore = pozole.orderPozole(meat);
+            PozoleRojoStore pozoleStore = new PozoleRojoStore();
+            pozolestore = pozoleStore.createPozole(meat);
         }
         if (getType().equals("Pozole Verde")) {
-            pozole = new PozoleVerdeStore();
-            pozolestore = pozole.orderPozole(meat);
+            PozoleVerdeStore pozoleStore = new PozoleVerdeStore();
+            pozolestore = pozoleStore.createPozole(meat);
         }
         if (getType().equals("Pozolillo")) {
-            pozole = new PozolilloStore();
-            pozolestore = orderPozole(meat);
+            PozolilloStore pozolilloStore = new PozolilloStore();
+            pozolestore = createPozole(meat);
         }
         return pozolestore;
     }
